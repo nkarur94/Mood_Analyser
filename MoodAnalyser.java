6be65bc1f1d763bf97e012c66eab6d6cdf422179
nithunkarur <rknithun@gmail.com>
2021-11-24 22:18:36 +0530

    ADDED:Mood analyser method to predict sad or happy

---
+++ MoodAnalyser.java
@@ -0,0 +1,12 @@
+package assignment21;
+
+public class MoodAnalyser {
+    public void moodAnalyse(String mood){
+        if(mood=="sad"){
+            System.out.println("SAD");
+        }
+        else{
+            System.out.println("HAPPY");
+        }
+    }
+}
---
+++ MoodCheck.java
@@ -0,0 +1,8 @@
+package assignment21;
+
+public class MoodCheck {
+    public static void main(String[] args) {
+        MoodAnalyser seeMood=new MoodAnalyser();
+        seeMood.moodAnalyse("happy");
+    }
+}
